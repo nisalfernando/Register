4ee17b6e7f904168956521d27937af4cc4350945
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-10-22 17:39:48 +0200

    Created the registration form

---
+++ register.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Validation</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="wrapper">
+  <form method="POST" action="index.php" onsubmit="return Validate()" name="vform">
+  	<div>
+  	  <input type="text" name="username" class="textInput" placeholder="Username">
+  	  <div id="name_error" class="val_error"></div>
+  	</div>
+  	<div>
+  	  <input type="email" name="email" class="textInput" placeholder="Email">
+  	  <div id="email_error" class="val_error"></div>
+  	</div>
+  	<div>
+  	  <input type="password" name="password" class="textInput" placeholder="Password">
+  	</div>
+  	<div>
+  	  <input type="password" name="password_confirmation" class="textInput" placeholder="Password confirmation">
+  	  <div id="password_error" class="val_error"></div>
+  	</div>
+  	<div>
+  	  <input type="submit" value="Register" class="btn" name="register">
+  	</div>
+  </form>	
+</div>
+
+</body>
+</html>
+
+<!-- JAVASCRIPT -->
+<script type="text/javascript">
+  // GETTING ALL INPUT TEXT OBJECTS
+  var username = document.forms["vform"]["username"];
+  var email = document.forms["vform"]["email"];
+  var password = document.forms["vform"]["password"];
+  var password_confirmation = document.forms["vform"]["password_confirmation"];
+
+  // GETTING ALL ERROR DISPLAY OBJECTS
+  var name_error = document.getElementById("name_error");
+  var email_error = document.getElementById("email_error");
+  var password_error = document.getElementById("password_error");
+
+  // SETTING ALL EVENT LISTENER
+  username.addEventListener("blur", nameVerify, true);
+  email.addEventListener("blur", emailVerify, true);
+  password.addEventListener("blur", passwordVerify, true);
+
+  // validation function
+  function Validate() {
+    // username validation
+    if (username.value == "") {
+      username.style.border = "1px solid red";
+      name_error.textContent = "Username is required";
+      username.focus();
+      return false;
+    }
+
+    // email validation
+    if (email.value == "") {
+      email.style.border = "1px solid red";
+      email_error.textContent = "Email is required";
+      email.focus();
+      return false;
+    }
+
+    // password validation
+    if (password.value == "") {
+      password.style.border = "1px solid red";
+      password_error.textContent = "Password is required";
+      password.focus();
+      return false;
+    }
+  }
+</script>
